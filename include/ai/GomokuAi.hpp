2e4b0ff02ee4ae9df7eaabf29e5f8362a36c6945
ZOZO <enzo.lemasson@epitech.eu>
2017-12-02 18:41:05 +0100

    enzo is pushing

--- include/ai/GomokuAi.hpp
+++ include/ai/GomokuAi.hpp
@@ -5,26 +5,42 @@
 #ifndef GOMUKO_GOMUKOAI_HPP
 #define GOMUKO_GOMUKOAI_HPP
 
-
+#include <vector>
+#include <array>
+#include <tuple>
 #include "AGomokuAi.hpp"
+
+#define PLAYER 1
+#define OPPONENT 2
+#define NONE 0
 
 class GomokuAi final : public AGomokuAi {
     /*
      * Variables
      */
-private:
 
+private:
+    typedef struct s_data
+    {
+        int play;
+        int value;
+    }BoardData;
+
+    std::vector<std::vector<BoardData>> board;
 
     /*
      * Constructor and destructor
      */
+
 public:
     GomokuAi();
     virtual ~GomokuAi() = default;
-
+    void setScore(unsigned int, unsigned int);
+    void setSquare(unsigned int, unsigned int);
     /*
      * Methods
      */
+
 protected:
     int gomoStart(unsigned int size) override;
     int gomoTurn(unsigned int x, unsigned int y) override;
--- src/ai/GomokuAi.cpp
+++ src/ai/GomokuAi.cpp
@@ -2,15 +2,35 @@
 // Created by 53269 on 28/11/2017.
 //
 
+#include <vector>
+#include <tuple>
 #include "../../include/ai/GomokuAi.hpp"
 
 //TODO: gomoSend est à changer (correspond à la réponse attendu par le programme)
 int GomokuAi::gomoStart(unsigned int size) {
     gomoSendStart(true);
+
+    std::vector<BoardData> line;
+    BoardData data;
+    int i = 0;
+    data.play = NONE;
+    data.value = 0;
+    while (i < size)
+    {
+        line.push_back(data);
+        i++;
+    }
+    i = 0;
+    while (i < size)
+    {
+        board.push_back(line);
+        i++;
+    }
     return 0;
 }
 
 int GomokuAi::gomoTurn(unsigned int ennemyX, unsigned int ennemyY) {
+    board.at(ennemyX).at(ennemyY).play = OPPONENT;
     gomoSendTurn(1, 1);
     return 0;
 }
@@ -40,4 +60,148 @@ int GomokuAi::gomoBoard(std::vector<std::string> &newBoard) {
 
 GomokuAi::GomokuAi() {
     about = R"(name="gomukobrain", version="1.0", author="Devia", country="FR")";
+}
+
+void GomokuAi::setScore(unsigned int x, unsigned int y)
+{
+    unsigned int i = y - 2;
+    unsigned int j;
+    while (i < y + 3)
+    {
+        j = x - 2;
+        while (j < x + 3)
+        {
+            if (j < board.size() && i < board.size() && j > 0 && i > 0 && board.at(i).at(j).play == NONE)
+                setSquare(i, j);
+            j++;
+        }
+        i++;
+    }
+}
+
+void GomokuAi::setSquare(unsigned int i, unsigned int j) {
+    unsigned int k;
+    unsigned int len;
+    int values = 0;
+    unsigned int r;
+    unsigned int l;
+    bool right;
+    bool left;
+
+    k = 0;
+    while (k < 4)
+    {
+        r = 0;
+        l = 0;
+        len = 1;
+        right = true;
+        left = true;
+        while (len < 5)
+        {
+            switch (k)
+            {
+                case 0:
+                    if (right && j + len >= board.size())
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (right && board.at(i).at(j + len).play == OPPONENT)
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (left && j - len < 0)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    if (left && board.at(i).at(j - len).play == OPPONENT)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    break;
+                case 1:
+                    if (right && (i + len >= board.size() || j + len >= board.size()))
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (right && board.at(i + len).at(j + len).play == OPPONENT)
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (left && (i - len < 0 || j - len < 0))
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    if (left && board.at(i - len).at(j - len).play == OPPONENT)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    break;
+                case 2:
+                    if (right && i + len >= board.size())
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (right && board.at(i + len).at(j).play == OPPONENT)
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (left && i - len < 0)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    if (left && board.at(i - len).at(j).play == OPPONENT)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    break;
+                case 3:
+                    if (right && (i - len < 0 || j + len >= board.size()))
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (right && board.at(i - len).at(j + len).play == OPPONENT)
+                    {
+                        r = len;
+                        right = false;
+                    }
+                    if (left && (i + len >= board.size() || j - len < 0))
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    if (left && board.at(i + len).at(j - len).play == OPPONENT)
+                    {
+                        l = len;
+                        left = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            len++;
+        }
+        if (right)
+            r = 4;
+        if (left)
+            l = 4;
+        if (r + l + 1 > 4)
+            values += r + l + 1;
+        else
+            values += 0;
+        k++;
+    }
+    board.at(i).at(j).value = values;
 }
